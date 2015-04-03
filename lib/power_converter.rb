5e6c0f19d653062732508ed12a74bed7dc36606f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-03 13:48:07 -0400

    Improving documentation

--- lib/power_converter.rb
+++ lib/power_converter.rb
@@ -5,13 +5,22 @@ require "power_converter/version"
 #
 # What is a conversion method?
 #
-# TODO: Define conversion method intent
+# > A well-established Ruby idiom for methods which “do the right thing” to
+# > convery any reasonable input value into a desired class.
+# >
+# > http://devblog.avdi.org/2012/05/07/a-ruby-conversion-idiom/
+#
+# Why conversion methods?
+#
+# Because software is all about addressing a mapping problem. In my experience
+# it has also exposed a means for easing the movement across application design
+# boundaries.
 module PowerConverter
   # When you tried to find a named_converter and it did not exist, this is a
   # reasonable exception to expect.
   class ConverterNotFoundError < RuntimeError
-    def initialize(named_converter, registered_converter_names)
-      super("Unable to find PowerConverter for #{named_converter} in #{registered_converter_names.inspect}")
+    def initialize(named_converter, defined_converter_names)
+      super("Unable to find PowerConverter for #{named_converter} in #{defined_converter_names.inspect}")
     end
   end
 
@@ -20,11 +29,18 @@ module PowerConverter
   # @api public
   # @since 0.0.1
   #
+  # Responsible for defining a conversion method and a "shovel-ready" conversion
+  # module; because maybe you want a mixin for convenience reasons.
+  #
   # @param named_conversion [String,Symbol] the name of the conversion that you
   #   are declaring.
+  # @param converter [#call] the callable object that will perform the
+  #   conversion.
   # @yield [value] A block that will be used to convert the given value
   #   to the named thing.
   # @yieldreturn returns the named thing.
+  #
+  # @return void
   #
   # @example
   #   PowerConverter.define_conversion_for :boolean do |input|
@@ -34,13 +50,24 @@ module PowerConverter
   #       true
   #     end
   #   end
+  # @see http://devblog.avdi.org/2012/05/07/a-ruby-conversion-idiom/ Avdi
+  #   Grimm's post on "A Ruby Conversion Idiom"
+  #
+  # @see Kernel#Array for inspiration
+  #
+  # @note The conversion module/method that is created may not adhear to the
+  #   exact idiom (a method defined in CamelCase)
+  #
+  # @todo Make sure that the converter requires at least one parameter.
   def define_conversion_for(named_conversion, &converter)
-    @conversions ||= {}
-    @conversions[named_conversion.to_s] = converter
+    @defined_conversions ||= {}
+    @defined_conversions[named_conversion.to_s] = converter
   end
 
   # @api public
   # @since 0.0.1
+  #
+  # Convert the given `value` via the named `:to` converter.
   #
   # @param value [Object] the thing that you will be converting
   # @param [Hash] options the options used to perform the conversion
@@ -52,12 +79,21 @@ module PowerConverter
   #
   # @example
   #   PowerConverter.convert('true', to: :boolean)
+  #
+  # @todo I want to:
+  #   * raise an exception if I'm unable to convert an object
+  #   * auto-handle :to_<named_conversion> so I don't need to worry about
+  #     registering that.
   def convert(value, options = {})
     converter_for(options.fetch(:to)).call(value)
   end
 
   # @api public
   # @since 0.0.1
+  #
+  # The means for mixing in a private conversion method; Perhaps as policy you
+  # don't want to expose the public conversion method but instead prefer to
+  # leverage private methods.
   #
   # @param named_conversion [String,Symbol] the name of the conversion that you
   #   are requesting be wrapped in a conversion module.
@@ -83,6 +119,8 @@ module PowerConverter
   # @api public
   # @since 0.0.1
   #
+  # Given the `named_conversion` find and retrieve the defined converter.
+  #
   # @param named_conversion [String,Symbol]
   #
   # @return [#call] a registered converter
@@ -94,16 +132,19 @@ module PowerConverter
   # @example
   #   PowerConverter.converter_for(:boolean).call(value)
   def converter_for(named_conversion)
-    @conversions.fetch(named_conversion.to_s)
+    @defined_conversions.fetch(named_conversion.to_s)
   rescue KeyError
-    raise ConverterNotFoundError.new(named_conversion, registered_converter_names)
+    raise ConverterNotFoundError.new(named_conversion, defined_converter_names)
   end
 
   # @api public
   # @since 0.0.1
   #
+  # A convenience method for seeing the names of all converters that have been
+  # defined.
+  #
   # @return [Array] of the registered converter's names
-  def registered_converter_names
-    @conversions.keys
+  def defined_converter_names
+    @defined_conversions.keys
   end
 end
