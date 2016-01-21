53d0552c1709a9daa35c9149000ec66cc8be9e83
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-01-21 16:03:28 -0500

    Open testing to more travis versions

--- lib/power_converter.rb
+++ lib/power_converter.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "power_converter/version"
 
 # PowerConverter is a composition service module. It provides a way to define
--- test/power_converter_test.rb
+++ test/power_converter_test.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'simplecov'
 if ENV['TRAVIS']
   require "codeclimate-test-reporter"
